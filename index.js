5217f3034e3d04f752b3d8806ddd711d223eb0b6
ZachLayman <zlayman@oakland.edu>
2021-12-03 17:59:06 -0500

    ejs

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const mysql = require("mysql");
+const ejs = require("ejs");
+
+const app = express()
+// Render static files
+app.use(express.static('public'));
+  
+  // Initialize Body Parser Middleware to parse data sent by users in the request object
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true })); // to parse HTML form data
+  
+  // Initialize ejs Middleware
+  app.set("view engine", "ejs");
+  app.use("/public", express.static(__dirname + "/public"));
+
+  // routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+  // Setup server ports
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
