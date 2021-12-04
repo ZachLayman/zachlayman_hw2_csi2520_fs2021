59d8fc149a284a4ee67baa877f30fded0d89a5f2
ZachLayman <zlayman@oakland.edu>
2021-12-04 18:15:01 -0500

    ejs

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ app.set("view engine", "ejs");
 app.use("/public", express.static(__dirname + "/public"));
 
 // routes
-//app.get("/info", (req, res) => {
-  //res.render("/view/info");
+//app.get("/", (req, res) => {
+  //res.render("index.ejs");
 //});
 
   // framework
