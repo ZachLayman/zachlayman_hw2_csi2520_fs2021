4f9c90fd969a36345361dc396de036faa17dcc7b
ZachLayman <zlayman@oakland.edu>
2021-12-03 20:39:31 -0500

    Local server working

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const express = require("express")
 const ejs = require("ejs");
-// const mysql = require("mysql");
 
-const app = express()
+const app = new express()
 // Render static files
 app.use(express.static('public'));
   
-  // Initialize Body Parser Middleware to parse data sent by users in the request object
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true })); // to parse HTML form data
-  
-  // Initialize ejs Middleware
-  app.set("view engine", "ejs");
-  app.use("/public", express.static(__dirname + "/public"));
 
   // routes
 app.get("/", (req, res) => {
-  res.render("node index");
+  res.send('hello from my express framework');
 });
 
 // Setup server ports
